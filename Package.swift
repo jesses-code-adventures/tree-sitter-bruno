bccfebde0c9f1894acf681e90179d657e8ac11a6
Jesse Williams <jesse@negativespacesounds.com>
2025-04-12 13:39:45 +1000

    commit generated code

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.3
+
+import Foundation
+import PackageDescription
+
+var sources = ["src/parser.c"]
+if FileManager.default.fileExists(atPath: "src/scanner.c") {
+    sources.append("src/scanner.c")
+}
+
+let package = Package(
+    name: "TreeSitterBruno",
+    products: [
+        .library(name: "TreeSitterBruno", targets: ["TreeSitterBruno"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.8.0"),
+    ],
+    targets: [
+        .target(
+            name: "TreeSitterBruno",
+            dependencies: [],
+            path: ".",
+            sources: sources,
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterBrunoTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterBruno",
+            ],
+            path: "bindings/swift/TreeSitterBrunoTests"
+        )
+    ],
+    cLanguageStandard: .c11
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jesses-code-adventures/tree-sitter-bruno
+
+go 1.22
+
+require github.com/tree-sitter/go-tree-sitter v0.24.0
